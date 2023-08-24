903f32d52fbffc90eb413277942b98a2672baf39
Martin <68254461+littlee5521@users.noreply.github.com>
2023-08-24 16:38:12 -0700

    test added for addition and passes. math functions will be in a object

---
+++ source/mathOperations.js
@@ -0,0 +1,7 @@
+const mathOperations = {
+  add: function(op1, op2) {
+    return op1 + op2
+  }
+}
+
+module.exports = mathOperations 
--- test/stringManipulation.test.js
+++ test/stringManipulation.test.js
@@ -1,10 +1,15 @@
 const capitlizeString = require('../source/capitlizeString.js')
 const reverseString = require('../source/reverseString.js')
+const mathOperations = require('../source/mathOperations.js')
 
 test('capitlizes first letter in string', () => {
   expect(capitlizeString('hello there')).toBe('Hello there')
 })
 
-test('should reverse a string', () => {
+test(' reverse a string', () => {
   expect(reverseString('hello there')).toBe('ereht olleh')
+})
+
+test('adds two numbers ', () => {
+  expect(mathOperations.add(3, 9)).toBe(12)
 })
